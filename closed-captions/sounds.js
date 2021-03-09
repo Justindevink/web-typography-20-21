ddb9e9c715d40059206d425968afb276871d4139
Justin de Vink <55492488+Justindevink@users.noreply.github.com>
2021-03-09 16:29:46 +0100

    alarm geluid effecten
    alarm geluid effecten toegevoegd met timestamps en keyframes

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,13 +1,15 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
 sounds = [
-	0,
-	9,
-	12,
-	15,
-	16.5,
-	24,
-	25.8,
+	0, //sound0
+	9, //sound1 geluid1
+	10, //sound2 geluid1
+	12, //sound3 geluid2
+	13, //sound4 geluid2
+	15, //sound5 geluid3
+	16, //sound6 geluid3
+	16.1, //sound7 geluid4
+	17, //sound8 geluid4
 	31,
 	32,
 	34,
