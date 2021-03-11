9b4901b363401676dc56afd47f1e8769fa2e46df
Justin de Vink <55492488+Justindevink@users.noreply.github.com>
2021-03-11 12:22:24 +0100

    geluidseffecten verder uitgewerkt

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,17 +1,22 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
 sounds = [
-	0, //sound0
-	9, //sound1 geluid1
-	10, //sound2 geluid1
-	12, //sound3 geluid2
-	13, //sound4 geluid2
-	15, //sound5 geluid3
-	16, //sound6 geluid3
-	16.1, //sound7 geluid4
-	17, //sound8 geluid4
-	31,
-	32,
-	34,
+	0, 		//sound0
+	9, 		//sound1 geluid1
+	10, 	//sound2 geluid1
+	12, 	//sound3 geluid2
+	13, 	//sound4 geluid2
+	15, 	//sound5 geluid3
+	16, 	//sound6 geluid3
+	16.1, 	//sound7 geluid4
+	17, 	//sound8 geluid4
+	24.5, 	//sound9 geluid5
+	25, 	//sound10 geluid5
+	25.5, 	//sound11 geluid6
+	26, 	//sound12 geluid6
+	31, 	//sound13 geluid7
+	32,		//sound14 geluid7
+	32.1,	//sound15 geluid8
+	33,		//sound16 geluid8
 	86
 ];
